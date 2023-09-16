4e229d9b08a5802b9d07b7cde833710c2d0c8199
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-09-16 15:55:18 -0700

    Initial Commit

---
+++ Pj10.08/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pj10.08
+//
+//  Created by Jacob Yeung on 9/16/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
